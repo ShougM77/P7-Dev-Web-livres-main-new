f6ef460da01b2d11a2a7732b8d1755604259357d
ShougM77 <mdge@hotmail.fr>
2024-10-09 23:00:07 +0200

    model ok

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const userSchema = mongoose.Schema(
-{
-    email : {type: String, required: true, unique: true},       //adresse e-mail de l’utilisateur [unique]
-    password : {type: String, required: true}                   //mot de passe haché de l’utilisateur
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true },    // email de l'utilisateur [unique]
+    password: { type: String, required: true }                // mot de passe haché de l'utilisateur
 });
 
 userSchema.plugin(uniqueValidator);
